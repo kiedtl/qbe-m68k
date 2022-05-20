f5ccea76853595c1ba09a575ada73aaf5ad71932
Kiëd Llaentenn <kiedtl@tilde.team>
2022-05-20 17:41:12 +0000

    feat: add bells and whistles to test runner

--- m68k/test/dotests.sh
+++ m68k/test/dotests.sh
@@ -4,6 +4,33 @@
 
 mkdir -p "test/funcs/obj"
 
+S_ERR="\x1b[31mNAH\x1b[m"
+ts() {
+    chars=("-" "\\" "|" "/")
+
+    c="+"
+    esc1=""
+    esc2=""
+
+    case "$1" in
+        -1) ;;
+        0)
+            esc1="\x1b[A\r\x1b[K"
+            esc2="\x1b[1m"
+        ;;
+        *)
+            c=${chars[$(($1 % ${#chars[*]}))]}
+            esc1="\x1b[A\r\x1b[K"
+        ;;
+    esac
+
+    status="$2"
+    status="$(printf '%b' "$status")"
+
+    printf '%b%s %b%-65s\x1b[m%10s\n' \
+        "$esc1" "$c" "$esc2" "$name..." "$status"
+}
+
 # _cproc SRC DEST
 _cproc() {
     cpp -Itest/ -I../third_party/libqbe "$1" | cproc-qbe -tm68k >| "$2"
@@ -26,11 +53,6 @@ _vlink() {
     vlink -brawbin1 -evector_table -o "$bin" $@
 }
 
-S_ERR="\x1b[31mERR\x1b[m"
-test_print() {
-    printf '\x1b[A\r\x1b[1m%-75s\x1b[m%b\n' "${1}..." "${2}"
-}
-
 for f in 'test/funcs/'*
 do
     # Reset variables that can be defined in the header of test funcs:
@@ -43,24 +65,24 @@ do
     name="$(basename "$f")"
     name="${name%%.*}"
 
-    printf '%-75s\n' "${name}..."
+    ts -1 "init"
 
     ssa="test/funcs/obj/${name}.ssa"
     asm="test/funcs/obj/${name}.asm"
     obj="test/funcs/obj/${name}.o"
     bin="test/funcs/obj/${name}.bin"
-    harness_asm="test/funcs/obj/harness-${name}.asm"
-    harness_obj="test/funcs/obj/harness-${name}.o"
+    harn_asm="test/funcs/obj/harness-${name}.asm"
+    harn_obj="test/funcs/obj/harness-${name}.o"
 
     eval $(grep '^//\$' $f | sed 's/^\/\/\$ //g')
 
-    grep '^//\:' $f | sed 's/^\/\/\:[ ]*//g' >| $harness_asm
+    grep '^//\:' $f | sed 's/^\/\/\:[ ]*//g' >| $harn_asm
 
-    _cproc "$f"   "$ssa"                               || break
-    _qbe   "$ssa" "$asm"                               || break
-    _vasm  "$asm" "$obj"                               || break
-    _vasm  "$harness_asm" "$harness_obj"               || break
-    _vlink "$bin" 'test/rt/rt.o' "$harness_obj" "$obj" || break
+    ts 1 "cproc"   && (_cproc "$f"   "$ssa"                            || break)
+    ts 2 "qbe"     && (_qbe   "$ssa" "$asm"                            || break)
+    ts 3 "vasm"    && (_vasm  "$asm" "$obj"                            || break)
+    ts 4 "harness" && (_vasm  "$harn_asm" "$harn_obj"                  || break)
+    ts 5 "vlink"   && (_vlink "$bin" 'test/rt/rt.o' "$harn_obj" "$obj" || break)
 
     stderr_file=$(mktemp)
     stdout=$(sim "$bin" 2> "$stderr_file")
@@ -68,10 +90,9 @@ do
 
     if [[ "$expect_output" != "$stdout" ]]
     then
-        test_print $name $S_ERR
+        ts 0 $S_ERR
         printf "Unexpected output (got: %q, expected: %q)\n\n" \
             "$stdout" "$expect_output"
-        break
     fi
 
     actual_D0=()
@@ -82,11 +103,10 @@ do
 
     if [[ "$expect_D0" != "${actual_D0[*]}" ]]
     then
-        test_print $name $S_ERR
+        ts 0 $S_ERR
         printf "Unexpected D0 values (got: %q, expected: %q)\n\n" \
             "${actual_D0[*]}" "$expect_D0"
-        break
     fi
 
-    test_print $name "OK"
+    ts 0 "OK"
 done
